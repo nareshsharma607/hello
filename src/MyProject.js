f85c5e874b720aad09b89b7804ce00a8606514eb
nareshsharma607 <76953510+nareshsharma607@users.noreply.github.com>
2021-10-27 17:43:38 +0530

    Initial commit

---
+++ src/MyProject.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+function Myproject() {
+  return (
+    <div>
+      <h1>Codial</h1>
+      <p>My app is a social media app</p>
+      <ul>
+        <li>sign in </li>
+        <li>sign up</li>
+        <li>sign out</li>
+      </ul>
+    </div>
+  );
+}
+export default Myproject;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Create a functional component called MyProject that returns the following UI:
+
+// An h1 with the name of your project
+// A paragraph to give a brief description of your project
+// An ordered or unordered list to discuss 3 major skills used in this project
+import react from "react";
+import ReactDOM from "react-dom";
+import MyProject from "./MyProject";
+
+ReactDOM.render(<MyProject />, document.getElementById("root"));
